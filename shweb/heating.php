efb2368d0de060a0fae36ca6e85ac4ddcda16167
Dennis Afanassiev <den@dataart.com>
2011-04-24 19:11:48 +0400

    some initial stubs

---
+++ shweb/heating.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<form action="heating.php">
+	Show me last: 
+	<a href="?n=10">10 lines</a> |
+	<a href="?n=60">60 lines</a> |
+	other:
+	<input type="text" name="n" value="<select>"/>
+	<input type="submit" value="Go"/>
+</form>
+<hr/>
+<?
+if ($_REQUEST[n] == "") $n = 10; else $n = $_REQUEST[n];
+$output = `tail /home/den/shc/controller.log -n$n`;
+print "<pre>$output</pre>";
+?>
+</body>
+</html>
+

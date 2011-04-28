950d016929b637ec804ffb5a3ca5e5bd7a880b7b
Dennis Afanassiev <den@dataart.com>
2011-04-28 22:25:50 +0400

    encoding fixed

--- shweb/heating.php
+++ shweb/heating.php
@@ -1,4 +1,8 @@
 <html>
+<head>    
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />     
+  <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+</head>
 <body>
 <?include 'menu.php';?>
 <form action="heating.php">

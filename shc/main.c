0d4b08cd5080fffda5f83dd3c83c74c43191e334
Dennis Afanassiev <den@dataart.com>
2011-10-23 18:26:17 +0400

    1wire addresses fix

--- shc/main.c
+++ shc/main.c
@@ -48,14 +48,14 @@ enum SwitchStatus
 
 /* Temperature sensors */
 const char* heaterSensor 	= "28.0AB28D020000"; /* датчик ТЭН */
-const char* inputSensor 	= "28.EDEA6D020000"; /* жидкость на входе */
-const char* outputSensor 	= "28.18DB6D020000"; /* жидкость на выходе */
 const char* externalSensor 	= "28.0FF26D020000"; /* улица */
+const char* outputSensor 	= "28.18DB6D020000"; /* жидкость на выходе */
 const char* amSensor 		= "28.4BC66D020000"; /* комната для гостей (АМ) */
+const char* inputSensor 	= "28.EDEA6D020000"; /* жидкость на входе */
 const char* bedroomSensor 	= "28.99C68D020000"; /* спальня */
 const char* cabinetSensor 	= "28.B5DE8D020000"; /* кабинет */
 const char* kitchenSensor	= "28.AAC56D020000"; /* кухня */
-const char* childrenSmallSensor	= "28.EDEA6D020000"; /* детская (Ал) */
+const char* childrenSmallSensor	= "28.CFE58D020000"; /* детская (Ал) */
 
 const char* heaterSwitch	= "/mnt/1wire/3A.3E9403000000/PIO.A";
 const char* pumpSwitch		= "/mnt/1wire/3A.3E9403000000/PIO.B";
@@ -404,9 +404,11 @@ int main()
 	int pumpState = controlPump(outgoingFluidTemp);
 
 	// -- Individual rooms control
+	/* Not tested yet	
 	int i;
 	for (i=0; i<ROOMS_COUNT; i++)
 		controlRoom(&roomControlDescriptors[i], targetTemp);
+	*/
 	
 	// -- Dates: now and when to start heating next time by our arrival
 	char nowStr[60], onStr[60];

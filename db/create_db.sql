e6a6330fccdf3230cae867f10dadafbada2187d6
den <den@dataart.com>
2013-09-14 01:06:07 +0400

    db creation script

---
+++ db/create_db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE heating (
+	time 		DATETIME,
+	external 	DECIMAL,
+	fluid_in 	DECIMAL,
+	heater 		DECIMAL,
+	fluid_out 	DECIMAL,
+	bedroom 	DECIMAL,
+	kitchen 	DECIMAL,
+	PRIMARY KEY(time));

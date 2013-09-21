3cbe779824e71ad1050ac37a4349b620f17a3523
den <den@dataart.com>
2013-09-22 01:33:08 +0400

    drop stuff

--- db/heating_consumption.sql
+++
@@ -1,4 +0,0 @@
-SELECT 	DATE(time) as Date, AVG(external), MIN(external), MAX(external), AVG(control), 
-	SUM(heating)/60 as HeatingTime, SUM(heating)/60*9 as HeatingKWh 
-FROM 	heating 
-GROUP BY DATE(time);

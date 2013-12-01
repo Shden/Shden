b308d78bed411638b234e523e658c369a6c986d0
shden <den@dataart.com>
2013-12-01 15:15:22 +0000

    Summary information extended

--- db/sp_get_status.sql
+++ db/sp_get_status.sql
@@ -2,20 +2,34 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS SP_GET_STATUS;
 CREATE PROCEDURE SP_GET_STATUS() 
 BEGIN
-	SELECT	MAX(hh.external) AS MAX_EXT, 
-		MIN(hh.external) AS MIN_EXT, 
-		AVG(hh.external) AS AVG_EXT, 
-		MAX(hh.control) AS MAX_INT, 
-		MIN(hh.control) AS MIN_INT, 
-		AVG(hh.control) AS AVG_INT, 
+	SELECT	MAX(h24.external) AS MAX_EXT_H24, 
+		MIN(h24.external) AS MIN_EXT_H24, 
+		AVG(h24.external) AS AVG_EXT_H24, 
+		MAX(h24.control) AS MAX_INT_H24, 
+		MIN(h24.control) AS MIN_INT_H24, 
+		AVG(h24.control) AS AVG_INT_H24,
+		d30.MAX_EXT_D30,
+                d30.MIN_EXT_D30,
+                d30.AVG_EXT_D30,
+                d30.MAX_INT_D30,
+                d30.MIN_INT_D30,
+                d30.AVG_INT_D30,
 		current.external AS CUR_EXT, 
 		current.control AS CUR_INT,
 		presence.time AS PRESENCE_TIME,
 		presence.isin AS PRESENCE_ISIN
-	FROM 	heating hh 
+	FROM 	heating h24 
 	JOIN   	(SELECT h.external, h.control FROM heating h ORDER BY time desc LIMIT 1) current
 	JOIN	(SELECT	p.time, p.isin FROM presence p ORDER BY time desc LIMIT 1) presence
-	WHERE 	hh.time > DATE_SUB(NOW(), INTERVAL 1 DAY);
+	JOIN	(SELECT	MAX(external) AS MAX_EXT_D30,
+                	MIN(external) AS MIN_EXT_D30,
+                	AVG(external) AS AVG_EXT_D30,
+                	MAX(control) AS MAX_INT_D30,
+                	MIN(control) AS MIN_INT_D30,
+                	AVG(control) AS AVG_INT_D30
+		FROM heating 
+		WHERE time > DATE_SUB(NOW(), INTERVAL 30 DAY)) d30
+	WHERE 	h24.time > DATE_SUB(NOW(), INTERVAL 1 DAY);
 
 END//
 DELIMITER ;

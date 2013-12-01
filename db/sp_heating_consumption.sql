df6386b469d9d938ec3a4c8d89550d4fb60906f2
shden <den@dataart.com>
2013-12-01 16:16:00 +0000

    23 to 7 night tariff

--- db/sp_heating_consumption.sql
+++ db/sp_heating_consumption.sql
@@ -20,12 +20,12 @@ BEGIN
 		MAX(external) as MaxOutside, 
 		AVG(control) as Inside, 
 		SUM(heating)/60 as HeatingTotalTime, 
-		SUM(CASE WHEN HOUR(time)<8 THEN heating END)/60.0 as HeatingNightTime, 
-		SUM(CASE WHEN HOUR(time)>=8 THEN heating END)/60.0 as HeatingDayTime, 
-		SUM(CASE WHEN HOUR(time)<8 THEN heating END)/60.0 * 9.0 * nightTariff as NightCost,
-		SUM(CASE WHEN HOUR(time)>=8 THEN heating END)/60.0 * 9.0 * dayTariff as DayCost, 
-		SUM(CASE WHEN HOUR(time)<8 THEN heating END)/60.0 * 9.0 * nightTariff +
-		SUM(CASE WHEN HOUR(time)>=8 THEN heating END)/60.0 * 9.0 * dayTariff as TotalCost, 
+		SUM(CASE WHEN HOUR(time)<7 OR HOUR(time)>=23 THEN heating END)/60.0 as HeatingNightTime, 
+		SUM(CASE WHEN HOUR(time)>=7 AND HOUR(time)<23 THEN heating END)/60.0 as HeatingDayTime, 
+		SUM(CASE WHEN HOUR(time)<7 OR HOUR(time)>=23 THEN heating END)/60.0 * 9.0 * nightTariff as NightCost,
+		SUM(CASE WHEN HOUR(time)>=7 AND HOUR(time)<23 THEN heating END)/60.0 * 9.0 * dayTariff as DayCost, 
+		SUM(CASE WHEN HOUR(time)<7 OR HOUR(time)>=23 THEN heating END)/60.0 * 9.0 * nightTariff +
+		SUM(CASE WHEN HOUR(time)>=7 AND HOUR(time)<23 THEN heating END)/60.0 * 9.0 * dayTariff as TotalCost, 
 		SUM(heating)/60.0 * 9.0 as HeatingKWh 
 	FROM 	heating 
 	WHERE	time > startDate AND time <= endDate

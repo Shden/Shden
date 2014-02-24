eeee5e015e631c55038b2ccd92b91bd05d6af183
shden <den@dataart.com>
2014-02-24 14:38:54 +0400

    Working version

--- shweb/lighting.php
+++ shweb/lighting.php
@@ -9,34 +9,33 @@
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-	
+
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
-	
+
 	<!-- Shweb cutom styles -->
 	<link rel="stylesheet" href="css/shweb.css">
 </head>
 <body>
 	<?php include 'menu.php';
-	
+
 	if (isset($_REQUEST['changeStreetLightStatusTo'])) 
-	{	
+	{
 		$newStatus = $_REQUEST['changeStreetLightStatusTo'];
 		`echo $newStatus >> /mnt/1wire/3A.B8380D000000/PIO.A`;
 	}
-	
-	$streetLightStatus = `cat /mnt/1wire/3A.B8380D000000/PIO.A`;
+
+	$streetLightStatus = (int)`cat /mnt/1wire/3A.B8380D000000/PIO.A`;
 	?>
-	
+
 	<div class="container" align="center">
 		<h2>Управление освещением</h2>
-		<a href="?changeStreetLightStatusTo=<?=($streetLightStatus = 1) ? 0 : 1?>" 
-			class="btn <?=($streetLightStatus = 1) ? "btn-default" : "btn-warning"?> btn-lg" role="button">
-			<?=($streetLightStatus = 1) ? "Погасить" : "Зажечь"?> уличный фонарь
+		<a href="?changeStreetLightStatusTo=<?=($streetLightStatus == 1) ? 0 : 1?>" 
+			class="btn <?=($streetLightStatus == 1) ? "btn-default" : "btn-warning"?> btn-lg" role="button">
+			<?=($streetLightStatus == 1) ? "Погасить" : "Зажечь"?> уличный фонарь
 		</a>
 	</div>
-	
-	
+
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="https://code.jquery.com/jquery.js"></script>
 	<!-- Latest compiled and minified JavaScript -->

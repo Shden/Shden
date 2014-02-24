49fe131b7883dd17d13c7c693627af322f74067c
den <den@dataart.com>
2014-02-24 14:22:22 +0400

    Implemented

--- shweb/lighting.php
+++ shweb/lighting.php
@@ -18,9 +18,24 @@
 </head>
 <body>
 	<?php include 'menu.php';
-	$streetLight = `cat /mnt/1wire/3A.B8380D000000/PIO.A`;
-	print "<pre>$streetLight</pre>";
+	
+	if (isset($_REQUEST['changeStreetLightStatusTo'])) 
+	{	
+		$newStatus = $_REQUEST['changeStreetLightStatusTo'];
+		`echo $newStatus >> /mnt/1wire/3A.B8380D000000/PIO.A`;
+	}
+	
+	$streetLightStatus = `cat /mnt/1wire/3A.B8380D000000/PIO.A`;
 	?>
+	
+	<div class="container" align="center">
+		<h2>Управление освещением</h2>
+		<a href="?changeStreetLightStatusTo=<?=($streetLightStatus = 1) ? 0 : 1?>" 
+			class="btn <?=($streetLightStatus = 1) ? "btn-default" : "btn-warning"?> btn-lg" role="button">
+			<?=($streetLightStatus = 1) ? "Погасить" : "Зажечь"?> уличный фонарь
+		</a>
+	</div>
+	
 	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="https://code.jquery.com/jquery.js"></script>

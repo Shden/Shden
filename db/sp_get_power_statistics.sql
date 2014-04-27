6ba962644024f0d606230907ad920bc11674df3d
den <den@dataart.com>
2014-04-27 12:42:20 +0400

    power statistics fix

--- db/sp_get_power_statistics.sql
+++ db/sp_get_power_statistics.sql
@@ -21,23 +21,28 @@ BEGIN
 	JOIN   	(
 			SELECT COUNT(*) as LowVoltage 
 			FROM	power p 
-			WHERE	p.U1 < 207 OR 
+			WHERE 	(p.time > DATE_SUB(NOW(), INTERVAL 1 DAY)) AND
+				(p.U1 < 207 OR 
 				p.U2 < 207 OR 
-				p.U3 < 207 
+				p.U3 < 207) 
 		) lv
 	JOIN   	(
 			SELECT COUNT(*) as HighVoltage 
 			FROM	power p 
-			WHERE	p.U1 > 253 OR
+			WHERE 	(p.time > DATE_SUB(NOW(), INTERVAL 1 DAY)) AND
+				(p.U1 > 253 OR
 				p.U2 > 253 OR
-				p.U3 > 253
+				p.U3 > 253)
 		) hv
 	JOIN	(
 			SELECT COUNT(*) as OnOffs
 			FROM (
-				SELECT @last AS prev_voltage, @last:=U1 + U2 + U3 AS now_voltage FROM power ORDER BY time
+				SELECT 	@last AS prev_voltage, @last:=U1 + U2 + U3 AS now_voltage 
+				FROM 	power 
+				WHERE 	time > DATE_SUB(NOW(), INTERVAL 1 DAY)
+					AND prev_voltage != 0 AND now_voltage = 0
+				ORDER BY time
 			) AS sub
-			WHERE prev_voltage != 0 AND now_voltage = 0
 		) oo
 	WHERE 	time > DATE_SUB(NOW(), INTERVAL 1 DAY);
 END//

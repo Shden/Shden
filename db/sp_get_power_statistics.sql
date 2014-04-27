0716c14c2cb9a06ad1a858c04a2837f373ba4312
den <den@dataart.com>
2014-04-28 01:17:01 +0400

    check

--- db/sp_get_power_statistics.sql
+++ db/sp_get_power_statistics.sql
@@ -14,11 +14,12 @@ BEGIN
 		MAX(U3) AS U3_MAX,
 		AVG(U3) AS U3_AVG,
 		STD(U3) AS U3_STD,
-		SUM(CASE WHEN U1 < 207 OR U2 < 207 OR U3 < 207 THEN 1 END) AS LowVoltageMinutes,
-		SUM(CASE WHEN U1 > 253 OR U2 > 253 OR U3 > 253 THEN 1 END) AS HighVoltageMinutes,
-		oo.OnOffs AS CutOffsCount
+		SUM(CASE WHEN U1 < 207 OR U2 < 207 OR U3 < 207 THEN 1 ELSE 0 END) AS LowVoltageMinutes,
+		SUM(CASE WHEN U1 > 253 OR U2 > 253 OR U3 > 253 THEN 1 ELSE 0 END) AS HighVoltageMinutes,
+		/*oo.OnOffs AS CutOffsCount*/
+		SUM(CASE WHEN U1 + U2 + U3 = 0 AND @last !=0 THEN 1 ELSE 0 END) AS CutOffsCount, @last:=U1 + U2 + U3
 	FROM	power
-	JOIN	(
+/*	JOIN	(
 			SELECT COUNT(*) as OnOffs
 			FROM (
 				SELECT 	@last AS prev_voltage, @last:=U1 + U2 + U3 AS now_voltage 
@@ -27,7 +28,7 @@ BEGIN
 				ORDER BY time
 			) AS sub
 			WHERE prev_voltage != 0 AND now_voltage = 0
-		) oo
+		) oo*/
 	WHERE 	time > DATE_SUB(NOW(), INTERVAL 1 DAY);
 END//
 DELIMITER ;

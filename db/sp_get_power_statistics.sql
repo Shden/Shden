0325f92be28537aaa2c1930b8739e5cfd8a8e842
den <den@dataart.com>
2014-04-28 01:31:05 +0400

    cutoff minutes counter

--- db/sp_get_power_statistics.sql
+++ db/sp_get_power_statistics.sql
@@ -2,7 +2,8 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS SP_GET_POWER_STATISTICS;
 CREATE PROCEDURE SP_GET_POWER_STATISTICS() 
 BEGIN
-	SELECT	MIN(U1) AS U1_MIN,
+	SELECT	DATE(time) AS DATE,
+		MIN(U1) AS U1_MIN,
 		MAX(U1) AS U1_MAX,
 		AVG(U1) AS U1_AVG,
 		STD(U1) AS U1_STD,
@@ -16,20 +17,8 @@ BEGIN
 		STD(U3) AS U3_STD,
 		SUM(CASE WHEN U1 < 207 OR U2 < 207 OR U3 < 207 THEN 1 ELSE 0 END) AS LowVoltageMinutes,
 		SUM(CASE WHEN U1 > 253 OR U2 > 253 OR U3 > 253 THEN 1 ELSE 0 END) AS HighVoltageMinutes,
-		/*oo.OnOffs AS CutOffsCount*/
-		SUM(CASE WHEN U1 + U2 + U3 = 0 AND @last !=0 THEN 1 ELSE 0 END) AS CutOffsCount, @last:=U1 + U2 + U3
+		SUM(CASE WHEN U1 = 0 OR U2 = 0 OR U3 =0 THEN 1 ELSE 0 END) AS CutoffMinutes
 	FROM	power
-/*	JOIN	(
-			SELECT COUNT(*) as OnOffs
-			FROM (
-				SELECT 	@last AS prev_voltage, @last:=U1 + U2 + U3 AS now_voltage 
-				FROM 	power 
-				WHERE 	time > DATE_SUB(NOW(), INTERVAL 1 DAY)
-				ORDER BY time
-			) AS sub
-			WHERE prev_voltage != 0 AND now_voltage = 0
-		) oo*/
-	/*WHERE 	time > DATE_SUB(NOW(), INTERVAL 1 DAY);*/
 	GROUP BY DATE(time);
 END//
 DELIMITER ;

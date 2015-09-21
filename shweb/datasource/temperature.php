cce5675aabb8ea827db858a472d9969e8b06066f
den <denis.afanassiev@gmail.com>
2015-09-21 19:34:27 +0300

    iOs DS update

--- shweb/datasource/temperature.php
+++ shweb/datasource/temperature.php
@@ -1,18 +1,8 @@
 <?php 
-include 'sql2js.php';
+$outsideTemp = (float)`cat /home/den/Shden/appliances/outsideTemp`;
+$bedRoomTemp = (float)`cat /home/den/Shden/appliances/bedRoomTemp`;
 
 header('Content-type: application/json');
 
-$days = 1;
-if (isset($_REQUEST[days])) $days = $_REQUEST[days];
-
-echo SQL2JS(	
-	"SELECT CONCAT(DATE(time), ' ', HOUR(time), ':00') as time, " .
-	"AVG(external) as outTemp, " .
-	"AVG(bedroom) as bedRoomTemp " .
-	"FROM heating " .
-	"WHERE time > DATE_ADD(NOW(), INTERVAL -$days DAY) " .
-	"GROUP BY HOUR(time), DATE(time) " .
-	"ORDER BY DATE(time), HOUR(time);"
-);
+echo "[{'time':'2015-09-20 19:00','outTemp':'$outsideTemp','bedRoomTemp':'$bedRoomTemp'}]";
 ?>

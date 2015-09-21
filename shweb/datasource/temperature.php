1be8d8da94b304fed260297b917583a0859351d3
den <denis.afanassiev@gmail.com>
2015-09-21 19:40:51 +0300

    iOs DS update 2

--- shweb/datasource/temperature.php
+++ shweb/datasource/temperature.php
@@ -4,5 +4,5 @@ $bedRoomTemp = (float)`cat /home/den/Shden/appliances/bedRoomTemp`;
 
 header('Content-type: application/json');
 
-echo "[{'time':'2015-09-20 19:00','outTemp':'$outsideTemp','bedRoomTemp':'$bedRoomTemp'}]";
+echo "[{\"outTemp\":\"$outsideTemp\",\"bedRoomTemp\":\"$bedRoomTemp\"}]";
 ?>

b601ad7661a59db6e237b37a99115295bc0cbb60
den <denis.afanassiev@gmail.com>
2015-09-22 15:27:21 +0300

    rollback temperature.php

--- shweb/datasource/temperature.php
+++ shweb/datasource/temperature.php
@@ -1,8 +1,18 @@
 <?php 
-$outsideTemp = (float)`cat /home/den/Shden/appliances/outsideTemp`;
-$bedRoomTemp = (float)`cat /home/den/Shden/appliances/bedRoomTemp`;
+include 'sql2js.php';
 
 header('Content-type: application/json');
 
-echo "[{\"outTemp\":\"$outsideTemp\",\"bedRoomTemp\":\"$bedRoomTemp\"}]";
+$days = 1;
+if (isset($_REQUEST[days])) $days = $_REQUEST[days];
+
+echo SQL2JS(	
+	"SELECT CONCAT(DATE(time), ' ', HOUR(time), ':00') as time, " .
+	"AVG(external) as outTemp, " .
+	"AVG(bedroom) as bedRoomTemp " .
+	"FROM heating " .
+	"WHERE time > DATE_ADD(NOW(), INTERVAL -$days DAY) " .
+	"GROUP BY HOUR(time), DATE(time) " .
+	"ORDER BY DATE(time), HOUR(time);"
+);
 ?>

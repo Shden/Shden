e264364ac413f2db03c1156bcd91e621af0ce697
den <denis.afanassiev@gmail.com>
2015-09-22 17:56:50 +0300

    now & stat -> json

--- shweb/datasource/housestatus.php
+++ shweb/datasource/housestatus.php
@@ -2,7 +2,11 @@
 include 'sql2js.php';
 
 header('Content-type: application/json');
-	
-echo SQL2JS("CALL SP_GET_STATISTICS();");
 
+$outsideTemp = (float)`cat /home/den/Shden/appliances/outsideTemp`;
+$bedRoomTemp = (float)`cat /home/den/Shden/appliances/bedRoomTemp`;
+
+echo "{\"now\":{\"outTemp\":\"$outsideTemp\",\"bedRoomTemp\":\"$bedRoomTemp\"},\"statistics\":";
+echo SQL2JS("CALL SP_GET_STATISTICS();");
+echo "}"
 ?>

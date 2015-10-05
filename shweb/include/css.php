ac157992321494ff1bd1045a3cc6ef6e6056449a
den <denis.afanassiev@gmail.com>
2015-10-05 12:56:00 +0300

    forgotten includes

---
+++ shweb/include/css.php
@@ -0,0 +1,5 @@
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
---
+++ shweb/include/js.php
@@ -0,0 +1,4 @@
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://code.jquery.com/jquery.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>

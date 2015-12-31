ca56bd87251ffd255626d71b9efe0c07a84f7c1b
den <denis.afanassiev@gmail.com>
2015-12-31 21:27:48 +0300

    Charts added

---
+++ iOS/Brod App/Brod App/HeatingChartController.swift
@@ -0,0 +1,14 @@
+//
+//  HeatingChartController.swift
+//  Брод
+//
+//  Created by Dennis Afanassiev on 31/12/15.
+//  Copyright © 2015 Dennis Afanassiev. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+class HeatingChartController : UITableViewController {
+    
+}

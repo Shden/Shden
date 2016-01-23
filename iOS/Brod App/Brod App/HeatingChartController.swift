e2b0ab0a1be97968012c1a9a24ca272943d6ed23
den <denis.afanassiev@gmail.com>
2016-01-23 17:39:59 +0300

    Line chart sewn

--- iOS/Brod App/Brod App/HeatingChartController.swift
+++ iOS/Brod App/Brod App/HeatingChartController.swift
@@ -9,6 +9,25 @@
 import Foundation
 import Charts
 
-class HeatingChartController : UITableViewController {
+class HeatingChartController : UIViewController {
     
+    @IBOutlet weak var lineChart: LineChartView!
+    
+    override func viewDidLoad() {
+        
+        let xSeries: [String?] = [ "5", "10", "15", "200" ]
+        let y: [ChartDataEntry] = [
+            ChartDataEntry(value: 0, xIndex: 0),
+            ChartDataEntry(value: 1, xIndex: 1),
+            ChartDataEntry(value: 2, xIndex: 2),
+            ChartDataEntry(value: 3, xIndex: 3)]
+        let set1 = LineChartDataSet(yVals: y)
+        
+        let dataset: [LineChartDataSet] = [set1]
+        
+        let data = LineChartData(xVals: xSeries, dataSets: dataset)
+        
+        lineChart.data = data
+        
+    }
 }

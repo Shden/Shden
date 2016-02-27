e84282c5c0c9bf0de17e2975c1ab0fc6420dea11
den <denis.afanassiev@gmail.com>
2016-02-28 00:07:35 +0300

    js refresh

--- shweb/power-stat.php
+++ shweb/power-stat.php
@@ -65,75 +65,89 @@
 	<?php 
 	include 'menu.php';
 	include 'include/js.php';
-
-	$days = (isset($_REQUEST[days])) ? $_REQUEST[days] : 14;
 	?>
-	<h2>Статистика электросети, интервал в днях: <?=$days?></h2>
-	<a href="?days=1">Сутки</a> |
-	<a href="?days=2">Двое суток</a> |
-	<a href="?days=7">Неделя</a> |
-	<a href="?days=14">2 недели</a> |
-	<a href="?days=21">3 недели</a> |
-	<a href="?days=31">Месяц</a> 
+
+	<h2>Статистика электросети, интервал в днях: <span id="daysCount">...</span></h2>
+
+	<div class="btn-group" role="group">
+		<a href="javascript:refreshForm(1)" class="btn btn-default">Сутки</a>
+		<a href="javascript:refreshForm(2)" class="btn btn-default">Двое суток</a>
+		<a href="javascript:refreshForm(7)" class="btn btn-default">Неделя</a>
+		<a href="javascript:refreshForm(14)" class="btn btn-default">2 недели</a>
+		<a href="javascript:refreshForm(21)" class="btn btn-default">3 недели</a>
+		<a href="javascript:refreshForm(31)" class="btn btn-default">Месяц</a> 
+	</div>
+	<br/><br/>
 
 	<div id="spinner" class="spinner"></div>
 
 	<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js" charset="utf-8"></script>
-	<script type='text/javascript'>
-	$('#spinner').show();
-	var spinner = createSpinner('spinner');
+	<script>
 
-	var table = d3.select(".container")
-		.append("table")
-			.attr("class", "datatable");
-	
-	var head = table.append("tr");
-	head.append("th").attr("rowspan", 2).attr("class", "bb leftalign").text("Дата");
-	for (var i=1; i<=3; i++)
-		head.append("th").attr("colspan", 4).attr("class", "centeralign lb f"+i).text("Фаза "+i);
-	head.append("th").attr("colspan", 3).attr("class", "centeralign lb").text("Ошибки (минут)");
-	
-	var head = table.append("tr");
-	
-	for (var i=1; i<=3; i++)
-	{
-		head.append("th").attr("class", "bb lb f"+i).text("Min (V)");
-		head.append("th").attr("class", "bb f"+i).text("Max (V)");
-		head.append("th").attr("class", "bb f"+i).text("Avg (V)");
-		head.append("th").attr("class", "bb rb f"+i).text("STD");
-	}
-	head.append("th").attr("class", "bb").text("Низк.");
-	head.append("th").attr("class", "bb").text("Выс.");
-	head.append("th").attr("class", "bb").text("Откл.");
-	
-	d3.json("../datasource/powerstat.php?days=<?=$days?>", function(error, data) {
-		var row = table.selectAll("row")
-				.data(data)
-			.enter().append("tr");
-			
-		row.attr("class", function(d) { return d.CutoffMinutes > 0 ? "failure" : ""; })
-		
-		row.append("td").attr("class", "leftalign rb").text(function(d) { return d.DATE; });
-
-		for (i=1; i<=3; i++)
-		{
-			row.append("td")
-				.attr("class", function(d) { return checkVal(230 * 0.9, 230 * 1.1, d["U"+i+"_MIN"], "lb f"+i); })
-				.text(function(d) { return d["U"+i+"_MIN"]; });
-			row.append("td")
-				.attr("class", function(d) { return checkVal(230 * 0.9, 230 * 1.1, d["U"+i+"_MAX"], "f"+i); })
-				.text(function(d) { return d["U"+i+"_MAX"]; });
-			row.append("td").attr("class", "f"+i).text(function(d) { return d["U"+i+"_AVG"]; });
-			row.append("td").attr("class", "rb f"+i).text(function(d) { return d["U"+i+"_STD"]; });
-		}
-
-		row.append("td").text(function(d) { return d.LowVMinutes; });
-		row.append("td").text(function(d) { return d.HighVMinutes; });
-		row.append("td").text(function(d) { return d.CutoffMinutes; });
+	$(document).ready(function() {
+		refreshForm(14);
 	});
 	
-	spinner.stop();
-	$('#spinner').hide();
+	function refreshForm(days)
+	{	
+		$('#spinner').show();
+		var spinner = createSpinner('spinner');
+		
+		$('#daysCount').html(days);
+
+		$('.datatable').remove();
+		var table = d3.select(".container")
+			.append("table")
+				.attr("class", "datatable");
+	
+		var head = table.append("tr");
+		head.append("th").attr("rowspan", 2).attr("class", "bb leftalign").text("Дата");
+		for (var i=1; i<=3; i++)
+			head.append("th").attr("colspan", 4).attr("class", "centeralign lb f"+i).text("Фаза "+i);
+		head.append("th").attr("colspan", 3).attr("class", "centeralign lb").text("Ошибки (минут)");
+	
+		var head = table.append("tr");
+	
+		for (var i=1; i<=3; i++)
+		{
+			head.append("th").attr("class", "bb lb f"+i).text("Min (V)");
+			head.append("th").attr("class", "bb f"+i).text("Max (V)");
+			head.append("th").attr("class", "bb f"+i).text("Avg (V)");
+			head.append("th").attr("class", "bb rb f"+i).text("STD");
+		}
+		head.append("th").attr("class", "bb").text("Низк.");
+		head.append("th").attr("class", "bb").text("Выс.");
+		head.append("th").attr("class", "bb").text("Откл.");
+	
+		d3.json("../datasource/powerstat.php?days=" + days, function(error, data) {
+			var row = table.selectAll("row")
+					.data(data)
+				.enter().append("tr");
+			
+			row.attr("class", function(d) { return d.CutoffMinutes > 0 ? "failure" : ""; })
+		
+			row.append("td").attr("class", "leftalign rb").text(function(d) { return d.DATE; });
+
+			for (i=1; i<=3; i++)
+			{
+				row.append("td")
+					.attr("class", function(d) { return checkVal(230 * 0.9, 230 * 1.1, d["U"+i+"_MIN"], "lb f"+i); })
+					.text(function(d) { return d["U"+i+"_MIN"]; });
+				row.append("td")
+					.attr("class", function(d) { return checkVal(230 * 0.9, 230 * 1.1, d["U"+i+"_MAX"], "f"+i); })
+					.text(function(d) { return d["U"+i+"_MAX"]; });
+				row.append("td").attr("class", "f"+i).text(function(d) { return d["U"+i+"_AVG"]; });
+				row.append("td").attr("class", "rb f"+i).text(function(d) { return d["U"+i+"_STD"]; });
+			}
+
+			row.append("td").text(function(d) { return d.LowVMinutes; });
+			row.append("td").text(function(d) { return d.HighVMinutes; });
+			row.append("td").text(function(d) { return d.CutoffMinutes; });
+		});
+	
+		spinner.stop();
+		$('#spinner').hide();
+	}
 	
 	function checkVal(min, max, actual, defaultClass)
 	{

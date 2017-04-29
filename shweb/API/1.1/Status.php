d0d4b1c2777f9fc659c57b9a813a3c8019e5106d
den <denis.afanassiev@gmail.com>
2017-04-30 00:25:26 +0300

    Heating -> Climate.php

--- shweb/API/1.1/Status.php
+++ shweb/API/1.1/Status.php
@@ -1,11 +1,11 @@
 <?php
 require_once ('../../include/db.inc');
-//require_once ('Heating.php');
+require_once ('Climate.php');
 
-/** 
+/**
  *	House status API endpoint. This API is primarely works with overall house status, including status snaps for remote
  *	monitoring and status changing.
- */	
+ */
 Class Status
 {
 	/**
@@ -16,9 +16,9 @@ Class Status
 	public function GetHouseStatus()
 	{
 		global $conn;
-		
+
 		$res = $conn->query("SELECT	time, isin FROM presence ORDER BY time desc LIMIT 1;");
-		if ($r = $res->fetch_assoc()) 
+		if ($r = $res->fetch_assoc())
 		{
 			$isin = (integer)$r["isin"];
 			$starting = $r["time"];
@@ -26,12 +26,12 @@ Class Status
 
 		$outsideTemp = (float)`cat /home/den/Shden/appliances/outsideTemp`;
 		$bedRoomTemp = (float)`cat /home/den/Shden/appliances/bedRoomTemp`;
-		
-		$heating = new Heating;
-		
+
+		$climate = new Climate;
+
 		return array(
 					"climate" => array(
-						"outTemp" 	=> $outsideTemp, 
+						"outTemp" 	=> $outsideTemp,
 						"inTemp"	=> $bedRoomTemp
 					),
 					"mode" => array(
@@ -39,12 +39,12 @@ Class Status
 						"starting"	=> $starting
 					),
 					"tempStat" => array(
-						"day" 	=> $heating->GetTempStatistics(1),
-						"month" => $heating->GetTempStatistics(30)
+						"day" 	=> $climate->GetTempStatistics(1),
+						"month" => $climate->GetTempStatistics(30)
 					)
 				);
 	}
-	
+
 	/**
 	 * Change house mode to the mode provided.
 	 *
@@ -53,13 +53,13 @@ Class Status
 	public function SetHouseMode($changeStatusTo)
 	{
 		global $conn;
-		
+
 		if ($changeStatusTo != 1 && $changeStatusTo != 0)
 		{
 			throw new RestException(400, 'Invalid staus requested.');
 		}
 		$conn->query("CALL SP_CHANGE_PRESENCE($changeStatusTo);");
-		
+
 		return $this->GetHouseStatus();
 	}
 }

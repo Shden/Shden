e049675abfc515a465e71c7c58ce121c3fa9279e
Den Afanassiev <den@dataart.com>
2017-06-06 23:18:52 +0300

    13.5kW

--- shc/heating.js
+++ shc/heating.js
@@ -13,7 +13,7 @@ const EXIT_OK			= 0;
 const EXIT_FAILURE		= 1;
 
 const MAX_POWER			= 17250;	/* 25 * 230 * 3 */
-const HEATER_POWER		= 13000;
+const HEATER_POWER		= 13500;
 
 const CELCIUS			= '\u00B0C';
 const BUILD			= '0.4';

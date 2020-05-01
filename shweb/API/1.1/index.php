da012ddfc6a0971bd0685f33cd406ff225790f07
Den Afanassiev <den@dataart.com>
2020-05-02 00:50:25 +0300

    New RestServer

--- shweb/API/1.1/index.php
+++ shweb/API/1.1/index.php
@@ -11,7 +11,7 @@ require 'Repellers.php';
 spl_autoload_register(); // don't load our classes unless we use them
 
 $mode = 'debug'; // 'debug' or 'production'
-$server = new RestServer($mode);
+$server = new Jacwright\RestServer\RestServer($mode);
 // $server->refreshCache(); // uncomment momentarily to clear the cache if classes change in production mode
 
 $server->addClass('Status', '/status');

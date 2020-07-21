db0ccae7df79a8c98bacb212f8e559b35b68ad8c
den <denis.afanassiev@gmail.com>
2020-07-21 04:48:12 +0100

    1wire address update

--- appliances/link-appliances.sh
+++ appliances/link-appliances.sh
@@ -4,8 +4,8 @@ echo "Creating links to appliances..."
 ln -s /mnt/1wire/3A.4A370D000000/PIO.A balkonLight
 ln -s /mnt/1wire/3A.4A370D000000/PIO.B streetLight150
 ln -s /mnt/1wire/3A.B8380D000000/PIO.A streetLight250
-ln -s /mnt/1wire/3A.2DE00E000000/PIO.B mainsSwitch
-ln -s /mnt/1wire/3A.2DE00E000000/PIO.A fenceLight
+ln -s /mnt/1wire/3A.35EE0E000000/PIO.B mainsSwitch
+ln -s /mnt/1wire/3A.35EE0E000000/PIO.A fenceLight
 ln -s /mnt/1wire/28.0FF26D020000/temperature outsideTemp
 ln -s /mnt/1wire/28.99C68D020000/temperature bedRoomTemp
 ln -s /mnt/1wire/28.AAC56D020000/temperature kitchenTemp

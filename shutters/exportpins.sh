a613f45289eefadb1aa0010e1604c5a7da22cd2d
Den Afanassiev <den@dataart.com>
2020-10-28 15:09:03 +0300

    exportpins.sh

--- shutters/exportpins.sh
+++ shutters/exportpins.sh
@@ -3,8 +3,11 @@
 # Common path for all GPIO access
 BASE_GPIO_PATH=/sys/class/gpio
 
-echo "22" > $BASE_GPIO_PATH/export
-echo "out" > $BASE_GPIO_PATH/gpio22/direction
-
 echo "6" > $BASE_GPIO_PATH/export
 echo "out" > $BASE_GPIO_PATH/gpio6/direction
+
+echo "13" > $BASE_GPIO_PATH/export
+echo "out" > $BASE_GPIO_PATH/gpio13/direction
+
+echo "20" > $BASE_GPIO_PATH/export
+echo "out" > $BASE_GPIO_PATH/gpio20/direction

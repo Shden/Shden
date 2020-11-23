4f6a8266c61264d85bccae2c8f17b8d2c267e731
Den Afanassiev <den@dataart.com>
2020-11-23 21:05:48 +0300

    configurable API versions, status refactored

--- shweb/test/statusAPI-tests.js
+++ shweb/test/statusAPI-tests.js
@@ -1,13 +1,21 @@
 var should = require('should');
 var http = require('http');
-var moment = require('moment');
 
-describe('/API/1.1/status testing:', function() {
+// -- figure out what API version to test
+process.argv.length.should.be.above(3);
+process.argv[2].should.be.String();
+let API_version = process.argv[2].split('=')[1];
+let APIconfig = require('./api-origin-config.json')['API_' + API_version];
+console.log(`API configuration:`);
+console.log(APIconfig);
 
-	it('GetHouseStatus', function(done) {
+describe(`/API/${APIconfig.version}/status testing:`, function() {
+
+	it(`GetHouseStatus: GET /API/${APIconfig.version}/status/GetHouseStatus`, function(done) {
 		http.get({
-			host: 'localhost',
-			path: '/API/1.1/status/GetHouseStatus'
+			host: APIconfig.host,
+			path: `/API/${APIconfig.version}/status/GetHouseStatus`,
+			port: APIconfig.port
 		}, function(responce) {
 			responce.statusCode.should.be.equal(200);
 			var data = '';
@@ -17,23 +25,26 @@ describe('/API/1.1/status testing:', function() {
 			});
 			responce.on('end', function() {
 				var status = JSON.parse(data);
-				// schedule.should.have.property("from");
-				// schedule.should.have.property("to");
-				// schedule.should.have.property("active");
-				// moment(schedule.from, moment.ISO_8601).isValid().should.be.ok();
-				// moment(schedule.to, moment.ISO_8601).isValid().should.be.ok();
+				// console.log(status)
+
+				status.should.be.an.Object();
+				status.should.have.property('climate');
+				status.should.have.property('mode');
+				status.should.have.property('power');
+
 				done();
 			});
 		});
 
 	});
 
-	describe('SetHouseMode tests:', function() {
+	describe(`SetHouseMode tests:`, function() {
 
 		function SM(m, expectedStatusCode, done) {
 			var req = http.request({
-				host: 'localhost',
-				path: `/API/1.1/status/SetHouseMode/${m}`,
+				host: APIconfig.host,
+				path: `/API/${APIconfig.version}/status/SetHouseMode/${m}`,
+				port: APIconfig.port,
 				method: 'PUT'
 			}, function(responce) {
 				responce.statusCode.should.be.equal(expectedStatusCode);
@@ -50,11 +61,11 @@ describe('/API/1.1/status testing:', function() {
 			req.end();
 		}
 
-		it('ON', function(done) {
+		it(`ON: PUT /API/${APIconfig.version}/status/SetHouseMode/1`, function(done) {
 			SM(1, 200, done);
 		});
 
-		it('OFF', function(done) {
+		it(`OFF: PUT /API/${APIconfig.version}/status/SetHouseMode/0`, function(done) {
 			SM(0, 200, done);
 		});
 

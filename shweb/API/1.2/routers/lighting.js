e6f530667fe76b5c1f3e3964aee58bb1c258a0b4
Den Afanassiev <den@dataart.com>
2020-11-30 19:59:13 +0300

    http-status-codes lib for http states

--- shweb/API/1.2/routers/lighting.js
+++ shweb/API/1.2/routers/lighting.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Lighting = require('../services/lighting');
+const HTTPStatus = require('http-status-codes');
 
-const HTTP_BAD_REQUEST = 400;
 
 /**
  *	Returns lighting status for all appliances connected to the system.
@@ -30,13 +30,13 @@ router.put('/ChangeStatus/:applianceName/:newStatus', async function(req, res)
 
         if (newStatus != 1 && newStatus != 0)
         {
-                res.status(HTTP_BAD_REQUEST).send(`Invalid status requested (${newStatus}).`);
+                res.status(HTTPStatus.BAD_REQUEST).send(`Invalid status requested (${newStatus}).`);
                 return;
         }
 
         if (applianceName.length < 3 || applianceName.length > 15)
         {
-                res.status(HTTP_BAD_REQUEST).send(`Invalid appliance name (${applianceName}).`);
+                res.status(HTTPStatus.BAD_REQUEST).send(`Invalid appliance name (${applianceName}).`);
                 return;
         }
 
--- shweb/API/1.2/routers/status.js
+++ shweb/API/1.2/routers/status.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const House = require('../services/house');
-
-const HTTP_BAD_REQUEST = 400;
+const HTTPStatus = require('http-status-codes');
 
 /**
  *	Return house status information.
@@ -26,12 +25,12 @@ router.put('/SetHouseMode/:changeStatusTo', async function(req, res) {
         let changeStatusTo = req.params.changeStatusTo;
         if (changeStatusTo != 1 && changeStatusTo != 0)
         {
-                res.status(HTTP_BAD_REQUEST).send(`Invalid status requested (${changeStatusTo}).`);
+                res.status(HTTPStatus.BAD_REQUEST).send(`Invalid status requested (${changeStatusTo}).`);
                 return;
         }
         
         let updatedStatus = await House.SetHouseMode(changeStatusTo);
-        res.status(200).send(JSON.stringify(updatedStatus)).end();
+        res.status(HTTPStatus.OK).send(JSON.stringify(updatedStatus)).end();
 });
 
 module.exports = router;

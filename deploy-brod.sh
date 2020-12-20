dbab612a57ff402afef19f6107a47b91f56fe114
Dennis Afanassiev <denis.afanassiev@gmail.com>
2020-12-20 22:00:38 +0300

    outdated

--- deploy-brod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Deploying HEAD version from github to brod..."
-
-ssh 85.26.213.178 -p 24861 "cd Shden && git pull"
--- testmercury.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1..100}
-do
-	curl 192.168.1.162:81/API/1.1/consumption/electricity/GetPowerMeterData
-done

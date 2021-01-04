5b0304fffb39264731209ccb7dd85b3bcaccaca7
Dennis Afanassiev <denis.afanassiev@gmail.com>
2021-01-04 22:26:05 +0300

    costmetics and formatting

--- shweb/config.php
+++ shweb/config.php
@@ -1,25 +1,24 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title>Настройки</title>
+		<title>Настройки</title>
 
-	<?php include 'include/css.php';?>
+		<?php include 'include/css.php';?>
 
-	<!-- Shweb cutom styles -->
-	<link rel="stylesheet" href="css/shweb.css">
-</head>
+		<!-- Shweb cutom styles -->
+		<link rel="stylesheet" href="css/shweb.css">
+	</head>
 
-<html>
 	<body>
 		<div class="container">
 			<?php
-			include 'menu.php';
-			include 'include/js.php';
-			?>
+				include 'menu.php';
+				include 'include/js.php';
+				?>
 
 			<div class="container col-sm-8">
 				<form>
--- shweb/schedule.php
+++ shweb/schedule.php
@@ -1,218 +1,221 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  	<title>Таймер отопления</title>
+		<title>Таймер отопления</title>
 
-	<?php include 'include/css.php';?>
+		<?php include 'include/css.php';?>
 
-	<!-- Shweb cutom styles -->
-	<link rel="stylesheet" href="css/shweb.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css">
+		<!-- Shweb cutom styles -->
+		<link rel="stylesheet" href="css/shweb.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css">
 
-	<style>
-	#alert {
-		display: none;
-	}
-	</style>
-</head>
-<body>
+		<style>
+		#alert {
+			display: none;
+		}
+		</style>
+	</head>
+	<body>
 	<div class="container">
 
-    <?php include 'menu.php';?>
+		<?php include 'menu.php';?>
 
-		<h2>Таймер отопления</h2>
-		<form>
-			<div class="row mb-3">
-				<label class="col-sm-2 control-form-label">Состояние:</label>
-				<div class="col-sm-8">
-					<div class="checkbox">
-						<label>
-							<input type="checkbox" id="timerActive" onchange="enableContols();">Таймер активен
-						</label>
+		<div class="container col-sm-8">
+
+			<h2>Таймер отопления</h2>
+			<form>
+				<div class="row mb-3">
+					<label class="col-sm-2 control-form-label">Состояние:</label>
+					<div class="col-sm-8">
+						<div class="checkbox">
+							<label>
+								<input type="checkbox" id="timerActive" onchange="enableContols();">Таймер активен
+							</label>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="alert alert-warning" id="alert">
-				<strong></strong>
-			</div>
-			<div class="row mb-3">
-				<label class="col-sm-2 control-label" for="arrive_date">Приедем:</label>
-				<div class="date col-sm-6">
-					<input type="text" class="form-control" id="arrive_date"
-						data-date-week-start="1" data-date-language="ru"
-						data-date-autoclose="true" data-date-days-of-week-highlighted="0,6"
-						data-date-format="DD, d MM yyyy"/>
+				<div class="alert alert-warning" id="alert">
+					<strong></strong>
 				</div>
-				<div class="col-sm-2">
-					<select class="form-control" id="arrive_hour"></select>
+				<div class="row mb-3">
+					<label class="col-sm-2 control-label" for="arrive_date">Приедем:</label>
+					<div class="date col-sm-6">
+						<input type="text" class="form-control" id="arrive_date"
+							data-date-week-start="1" data-date-language="ru"
+							data-date-autoclose="true" data-date-days-of-week-highlighted="0,6"
+							data-date-format="DD, d MM yyyy"/>
+					</div>
+					<div class="col-sm-2">
+						<select class="form-control" id="arrive_hour"></select>
+					</div>
 				</div>
-			</div>
-			<div class="row mb-3">
-				<label class="col-sm-2 control-label" for="dep_date">Уедем:</label>
-				<div class="date col-sm-6">
-					<input type="text" class="form-control" id="dep_date"
-						data-date-week-start="1" data-date-language="ru"
-						data-date-autoclose="true" data-date-days-of-week-highlighted="0,6"
-						data-date-format="DD, d MM yyyy"/>
- 				</div>
-				 <div class="col-sm-2">
-					<select class="form-control" id="dep_hour"></select>
+				<div class="row mb-3">
+					<label class="col-sm-2 control-label" for="dep_date">Уедем:</label>
+					<div class="date col-sm-6">
+						<input type="text" class="form-control" id="dep_date"
+							data-date-week-start="1" data-date-language="ru"
+							data-date-autoclose="true" data-date-days-of-week-highlighted="0,6"
+							data-date-format="DD, d MM yyyy"/>
+					</div>
+					<div class="col-sm-2">
+						<select class="form-control" id="dep_hour"></select>
+					</div>
 				</div>
-			</div>
-			<div class="row mb-3">
-				<div class="col-sm-6">
-					<a class="btn btn-primary" href="javascript:updateSchedule()">Обновить настройки таймера</a>
+				<div class="row mb-3">
+					<div class="col-sm-6">
+						<a class="btn btn-primary" href="javascript:updateSchedule()">Обновить настройки таймера</a>
+					</div>
 				</div>
-			</div>
-		</form>
-	</div>
-	<div id="spinner" class="spinner">
+			</form>
+		</div>
 
-	<?php include 'include/js.php';?>
+		<div id="spinner" class="spinner">
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/locales/bootstrap-datepicker.ru.min.js"></script>
+		<?php include 'include/js.php';?>
 
-  	<script type="text/javascript">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/locales/bootstrap-datepicker.ru.min.js"></script>
 
-		$(document).ready(function()
-		{
-			initForm();
-			loadFormData();
-		});
+		<script type="text/javascript">
 
-		function initForm()
-		{
-			$('#arrive_date').datepicker('setDate', new Date);
-			$('#dep_date').datepicker('setDate', new Date);
-
-			/* Dates validation handlers */
-			$('#arrive_date').datepicker()
-				.on('changeDate', function(ev) {
-					if (ev.date.valueOf() > $('#dep_date').datepicker('getDate').valueOf()) {
-						$('#alert').show().find('strong').text('Предупреждение: дата прибытия должна быть раньше даты отъезда.');
-					} else {
-						$('#alert').hide();
-					}
-				});
-			$('#dep_date').datepicker()
-				.on('changeDate', function(ev) {
-					if (ev.date.valueOf() < $('#arrive_date').datepicker('getDate').valueOf()) {
-						$('#alert').show().find('strong').text('Предупреждение: дата отъезда должна быть после даты прибытия.');
-					} else {
-						$('#alert').hide();
-					}
-				});
-
-			for (var hour=0; hour<24; hour++)
+			$(document).ready(function()
 			{
-				$('#arrive_hour').append($('<option>', {
-				    value: hour,
-				    text: hour + ':00'
-				}));
-				$('#dep_hour').append($('<option>', {
-				    value: hour,
-				    text: hour + ':00'
-				}));
-			}
-		}
-
-		function loadFormData()
-		{
-			$('#spinner').show();
-			var spinner = createSpinner('spinner');
-
-			$.getJSON(GetAPIURL("climate/GetSchedule"))
-				.done(function(data) {
-
-					refreshControls(data);
-
-					spinner.stop();
-					$('#spinner').hide();
-				})
-				.fail(function() {
-					alert('Ошибка вызова GetSchedule.');
-				});
-		}
-
-		function refreshControls(data)
-		{
-			$('#timerActive').prop('checked', data.active == 1);
-
-			var arrivalDate, departureDate;
-
-			if (data.active == 1)
-			{
-				var arrivalDate = new Date(data.from);
-				var departureDate = new Date(data.to);
-			}
-			else
-			{
-				/* Setting default start and end dates to the next weekend (Friday to Sunday) */
-				arrivalDate = new Date();
-				while (arrivalDate.getDay() != 5)
-					arrivalDate.setDate(arrivalDate.getDate() + 1);
-
-				var departureDate = new Date();
-			        departureDate.setDate((arrivalDate.getDate() + 2));
-
-				arrivalDate.setHours(18);
-				departureDate.setHours(21);
-			}
-
-			$('#arrive_date').datepicker('setDate', arrivalDate).datepicker('update');
-			$('#dep_date').datepicker('setDate', departureDate).datepicker('update');
-
-			$('#arrive_hour').val(arrivalDate.getHours()).attr('selected', 'selected');
-			$('#dep_hour').val(departureDate.getHours()).attr('selected', 'selected');
-
-			enableContols();
-		}
-
-		function enableContols()
-		{
-			var active = $('#timerActive').prop('checked');
-
-			$('#arrive_date').prop('disabled', !active);
-			$('#arrive_hour').prop('disabled', !active);
-			$('#dep_date').prop('disabled', !active);
-			$('#dep_hour').prop('disabled', !active);
-		}
-
-		function updateSchedule()
-		{
-			var arr = $('#arrive_date').datepicker('getDate');
-			var dep = $('#dep_date').datepicker('getDate');
-
-			arr.setHours($('#arrive_hour').val());
-			dep.setHours($('#dep_hour').val());
-
-			var URL = ($('#timerActive').prop('checked'))
-				? GetAPIURL("climate/SetSchedule/" +
-					arr.getFullYear() + '/' + (arr.getMonth() + 1) + '/' + arr.getDate() + '/' + arr.getHours() + '/' +
-					dep.getFullYear() + '/' + (dep.getMonth() + 1) + '/' + dep.getDate() + '/' + dep.getHours())
-				: GetAPIURL("climate/ResetSchedule");
-
-			$('#spinner').show();
-			var spinner = createSpinner('spinner');
-
-	        	$.ajax({
-				url: URL,
-				type: 'PUT',
-				dataType: 'json',
-				success: function(data) {
-
-					refreshControls(data)
-
-					spinner.stop();
-					$('#spinner').hide();
-				}
+				initForm();
+				loadFormData();
 			});
-		}
-	</script>
-</body>
+
+			function initForm()
+			{
+				$('#arrive_date').datepicker('setDate', new Date);
+				$('#dep_date').datepicker('setDate', new Date);
+
+				/* Dates validation handlers */
+				$('#arrive_date').datepicker()
+					.on('changeDate', function(ev) {
+						if (ev.date.valueOf() > $('#dep_date').datepicker('getDate').valueOf()) {
+							$('#alert').show().find('strong').text('Предупреждение: дата прибытия должна быть раньше даты отъезда.');
+						} else {
+							$('#alert').hide();
+						}
+					});
+				$('#dep_date').datepicker()
+					.on('changeDate', function(ev) {
+						if (ev.date.valueOf() < $('#arrive_date').datepicker('getDate').valueOf()) {
+							$('#alert').show().find('strong').text('Предупреждение: дата отъезда должна быть после даты прибытия.');
+						} else {
+							$('#alert').hide();
+						}
+					});
+
+				for (var hour=0; hour<24; hour++)
+				{
+					$('#arrive_hour').append($('<option>', {
+					value: hour,
+					text: hour + ':00'
+					}));
+					$('#dep_hour').append($('<option>', {
+					value: hour,
+					text: hour + ':00'
+					}));
+				}
+			}
+
+			function loadFormData()
+			{
+				$('#spinner').show();
+				var spinner = createSpinner('spinner');
+
+				$.getJSON(GetAPIURL("climate/GetSchedule"))
+					.done(function(data) {
+
+						refreshControls(data);
+
+						spinner.stop();
+						$('#spinner').hide();
+					})
+					.fail(function() {
+						alert('Ошибка вызова GetSchedule.');
+					});
+			}
+
+			function refreshControls(data)
+			{
+				$('#timerActive').prop('checked', data.active == 1);
+
+				var arrivalDate, departureDate;
+
+				if (data.active == 1)
+				{
+					var arrivalDate = new Date(data.from);
+					var departureDate = new Date(data.to);
+				}
+				else
+				{
+					/* Setting default start and end dates to the next weekend (Friday to Sunday) */
+					arrivalDate = new Date();
+					while (arrivalDate.getDay() != 5)
+						arrivalDate.setDate(arrivalDate.getDate() + 1);
+
+					var departureDate = new Date();
+					departureDate.setDate((arrivalDate.getDate() + 2));
+
+					arrivalDate.setHours(18);
+					departureDate.setHours(21);
+				}
+
+				$('#arrive_date').datepicker('setDate', arrivalDate).datepicker('update');
+				$('#dep_date').datepicker('setDate', departureDate).datepicker('update');
+
+				$('#arrive_hour').val(arrivalDate.getHours()).attr('selected', 'selected');
+				$('#dep_hour').val(departureDate.getHours()).attr('selected', 'selected');
+
+				enableContols();
+			}
+
+			function enableContols()
+			{
+				var active = $('#timerActive').prop('checked');
+
+				$('#arrive_date').prop('disabled', !active);
+				$('#arrive_hour').prop('disabled', !active);
+				$('#dep_date').prop('disabled', !active);
+				$('#dep_hour').prop('disabled', !active);
+			}
+
+			function updateSchedule()
+			{
+				var arr = $('#arrive_date').datepicker('getDate');
+				var dep = $('#dep_date').datepicker('getDate');
+
+				arr.setHours($('#arrive_hour').val());
+				dep.setHours($('#dep_hour').val());
+
+				var URL = ($('#timerActive').prop('checked'))
+					? GetAPIURL("climate/SetSchedule/" +
+						arr.getFullYear() + '/' + (arr.getMonth() + 1) + '/' + arr.getDate() + '/' + arr.getHours() + '/' +
+						dep.getFullYear() + '/' + (dep.getMonth() + 1) + '/' + dep.getDate() + '/' + dep.getHours())
+					: GetAPIURL("climate/ResetSchedule");
+
+				$('#spinner').show();
+				var spinner = createSpinner('spinner');
+
+				$.ajax({
+					url: URL,
+					type: 'PUT',
+					dataType: 'json',
+					success: function(data) {
+
+						refreshControls(data)
+
+						spinner.stop();
+						$('#spinner').hide();
+					}
+				});
+			}
+		</script>
+	</body>
 </html>

7fa6995e1e358b71ce5ecc48c373c285617905b7
Dennis Afanassiev <denis.afanassiev@gmail.com>
2021-01-08 00:34:08 +0300

    config deep merge to fix a bug

--- houseAPI/gates/config-gate.js
+++ houseAPI/gates/config-gate.js
@@ -15,11 +15,40 @@ async function getConfig()
         });
 }
 
+/**
+ * Performs a deep merge of `source` into `target`.
+ * Mutates `target` only but not its objects and arrays.
+ *
+ * @author inspired by [jhildenbiddle](https://stackoverflow.com/a/48218209).
+ */
+function mergeDeep(target, source) {
+        const isObject = (obj) => obj && typeof obj === 'object';
+
+        if (!isObject(target) || !isObject(source)) {
+                return source;
+        }
+
+        Object.keys(source).forEach(key => {
+                const targetValue = target[key];
+                const sourceValue = source[key];
+
+                if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+                        target[key] = targetValue.concat(sourceValue);
+                } else if (isObject(targetValue) && isObject(sourceValue)) {
+                        target[key] = mergeDeep(Object.assign({}, targetValue), sourceValue);
+                } else {
+                        target[key] = sourceValue;
+                }
+        });
+
+        return target;
+}
+
 // Updates house configuration based on the requested new state
 async function updateConfig(newConfig)
 {
         let currentConfig = await getConfig();
-        let combinedConfig = { ...currentConfig, ...newConfig };
+        let combinedConfig = mergeDeep(currentConfig, newConfig);
 
         return new Promise((resolved, rejected) => {
                 fs.writeFile(configFile, JSON.stringify(combinedConfig), (err) => {

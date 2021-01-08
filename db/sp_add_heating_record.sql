8a60b9b223fef17c3dd62db5d2a822836b96ef15
Dennis Afanassiev <denis.afanassiev@gmail.com>
2021-01-08 18:11:30 +0300

    Recorder to include ESP datapoints

--- db/sp_add_heating_record.sql
+++ db/sp_add_heating_record.sql
@@ -4,53 +4,32 @@
 DELIMITER //
 DROP PROCEDURE IF EXISTS SP_ADD_HEATING_RECORD;
 CREATE PROCEDURE SP_ADD_HEATING_RECORD(
-	heater 			DECIMAL(5,2),	-- heater temperature
-	fluid_in		DECIMAL(5,2),	-- heater incoming fluid temperature
-	fluid_out		DECIMAL(5,2),	-- heater outgoing fluid temperature
-	external		DECIMAL(5,2),	-- street temperature
-	am_bedroom		DECIMAL(5,2),	-- AM bedroom temperature
-	bedroom			DECIMAL(5,2),	-- bedroom temperature
-	cabinet			DECIMAL(5,2),	-- office temperature
-	child_bedroom		DECIMAL(5,2),	-- kids bedroom temperature
-	kitchen			DECIMAL(5,2),	-- kitchen temperature
-	bathroom_1		DECIMAL(5,2),	-- sauna temperature
-	bathroom_1_floor	DECIMAL(5,2),	-- sauna floor temperature
-	control			DECIMAL(5,2),	-- control temperature
-	heatingOn		TINYINT,	-- is heater on
-	pumpOn			TINYINT,	-- is pump on
-	bathroom_1_heatingOn	TINYINT		-- is sauna floor heating on
+	heater 			DECIMAL(5,2),	-- 1 heater temperature
+	fluid_in		DECIMAL(5,2),	-- 2 heater incoming fluid temperature
+	fluid_out		DECIMAL(5,2),	-- 3 heater outgoing fluid temperature
+	external		DECIMAL(5,2),	-- 4 street temperature
+	am_bedroom		DECIMAL(5,2),	-- 5 AM bedroom temperature
+	bedroom			DECIMAL(5,2),	-- 6 bedroom temperature
+	cabinet			DECIMAL(5,2),	-- 7 office temperature
+	child_bedroom		DECIMAL(5,2),	-- 8 kids bedroom temperature
+	kitchen			DECIMAL(5,2),	-- 9 kitchen temperature
+	bathroom_1		DECIMAL(5,2),	-- 10 sauna temperature
+	bathroom_1_floor	DECIMAL(5,2),	-- 11 sauna floor temperature
+	control			DECIMAL(5,2),	-- 12 control temperature
+	hall_floor_1		DECIMAL(5,2),	-- 13 1st segment to windows
+	hall_floor_2		DECIMAL(5,2),	-- 14 2nd segment in the middle
+	hall_floor_3		DECIMAL(5,2)	-- 15 3rd small segment
 )
 BEGIN
-	DECLARE thisMinute, nextMinunte, tickTime DATETIME;
-
-	SELECT SUBTIME(NOW(), SEC_TO_TIME(SECOND(NOW()))) INTO thisMinute;
-	SELECT DATE_ADD(thisMinute, INTERVAL 1 MINUTE) INTO nextMinunte;
-	SELECT MIN(time) FROM heating WHERE time >= thisMinute AND time < nextMinunte INTO tickTime;
-
-	IF ISNULL(tickTime) = 1 THEN
-		INSERT INTO heating
-		(time, heater, fluid_in, fluid_out, external,
-		am_bedroom, bedroom, cabinet, sasha_bedroom,
-		kitchen, bathroom, sauna_floor, control, heating,
-		pump, sauna_heating)
-		VALUES (NOW(),
-		heater, fluid_in, fluid_out, external,
-		am_bedroom, bedroom, cabinet, child_bedroom,
-		kitchen, bathroom_1, bathroom_1_floor, control, heatingOn,
-		pumpOn, bathroom_1_heatingOn);
-
-	ELSE
-		UPDATE heating h
-		SET h.heater = heater, h.fluid_in = fluid_in,
-		h.fluid_out = fluid_out, h.external = external,
-		h.am_bedroom = am_bedroom, h.bedroom = bedroom,
-		h.cabinet = cabinet, h.sasha_bedroom = child_bedroom,
-		h.kitchen = kitchen, h.bathroom = bathroom_1,
-		h.sauna_floor = bathroom_1_floor, h.control = control,
-		h.heating = heatingOn, h.pump = pumpOn,
-		h.sauna_heating = bathroom_1_heatingOn
-		WHERE time = tickTime;
-	END IF;
-
+	INSERT INTO heating
+	(time, heater, fluid_in, fluid_out, external,
+	am_bedroom, bedroom, cabinet, sasha_bedroom,
+	kitchen, bathroom, sauna_floor, control,
+	hall_floor_1, hall_floor_2, hall_floor_3)
+	VALUES (NOW(),
+	heater, fluid_in, fluid_out, external,
+	am_bedroom, bedroom, cabinet, child_bedroom,
+	kitchen, bathroom_1, bathroom_1_floor, control,
+	hall_floor_1, hall_floor_2, hall_floor_3);
 END//
 DELIMITER ;
--- recorder/recorder.js
+++ recorder/recorder.js
@@ -15,7 +15,7 @@ setInterval(() => {
         
         thingAPI.getStatus().then(dataPoint => {
                 Promise.all([
-                        persistHeatingData(DBConnectionPool, dataPoint.oneWireStatus),
+                        persistHeatingData(DBConnectionPool, dataPoint),
                         persistHumidityData(DBConnectionPool, dataPoint.oneWireStatus),
                         persistPowerData(DBConnectionPool, dataPoint.powerStatus)
                 ])
@@ -35,22 +35,22 @@ function persistHeatingData(dbConnectionPool, dataPoint)
                         dbConnectionPool.query(
                                 "CALL SP_ADD_HEATING_RECORD(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", 
                                 [
-                                                                                                // SP_ADD_HEATING_RECORD params:
-                                        null,                                                   // 1 - heater (not used)
-                                        dataPoint.temperatureSensors.fluid_in,                  // 2 - fluid_in
-                                        dataPoint.temperatureSensors.fluid_out,                 // 3 - fluid_out
-                                        dataPoint.temperatureSensors.outsideTemp,               // 4 - external
-                                        dataPoint.temperatureSensors.am_bedroom,                // 5 - am_bedroom
-                                        dataPoint.temperatureSensors.bedroom,                   // 6 - bedroom
-                                        dataPoint.temperatureSensors.cabinet,                   // 7 - cabinet
-                                        dataPoint.temperatureSensors.child_bedroom,             // 8 - child_bedroom
-                                        dataPoint.temperatureSensors.kitchen,                   // 9 - kitchen
-                                        dataPoint.temperatureSensors.sauna_ceiling,             // 10 - bathroom_1
-                                        dataPoint.temperatureSensors.bathroom_1_floor_1,        // 11 - bathroom_1_floor
-                                        null,                                                   // 12 - control (not used)
-                                        null,                                                   // 13 - heatingOn (not used)
-                                        null,                                                   // 14 - pumpOn (not used)
-                                        dataPoint.switches.saunaFloorSwitch                     // 15 - bathroom_1_heatingOn
+                                                                                                        // SP_ADD_HEATING_RECORD params:
+                                        null,                                                           // 1 - heater (not used)
+                                        dataPoint.oneWireStatus.temperatureSensors.fluid_in,            // 2 - fluid_in
+                                        dataPoint.oneWireStatus.temperatureSensors.fluid_out,           // 3 - fluid_out
+                                        dataPoint.oneWireStatus.temperatureSensors.outsideTemp,         // 4 - external
+                                        dataPoint.oneWireStatus.temperatureSensors.am_bedroom,          // 5 - am_bedroom
+                                        dataPoint.oneWireStatus.temperatureSensors.bedroom,             // 6 - bedroom
+                                        dataPoint.oneWireStatus.temperatureSensors.cabinet,             // 7 - cabinet
+                                        dataPoint.oneWireStatus.temperatureSensors.child_bedroom,       // 8 - child_bedroom
+                                        dataPoint.oneWireStatus.temperatureSensors.kitchen,             // 9 - kitchen
+                                        dataPoint.oneWireStatus.temperatureSensors.sauna_ceiling,       // 10 - bathroom_1
+                                        dataPoint.oneWireStatus.temperatureSensors.bathroom_1_floor_1,  // 11 - bathroom_1_floor
+                                        null,                                                           // 12 - control (not used)
+                                        dataPoint.ESP.hall_floor_1,                                     // 13 - 1st segment to windows
+                                        dataPoint.ESP.hall_floor_1,                                     // 14 - 2nd segment in the middle
+                                        dataPoint.ESP.hall_floor_1                                      // 15 - 3rd small segment
                                 ]
                         ).then(() => {
                                 dbConnection.end();

bbbeddef4a6309b2a69c3b8175e0afee529f57a1
Dennis Afanassiev <denis.afanassiev@gmail.com>
2021-01-15 14:27:46 +0300

    lockfile to avoid concurrent writfile

--- houseAPI/gates/esp-gate.js
+++ houseAPI/gates/esp-gate.js
@@ -1,8 +1,10 @@
 // ESP controllers gate. Simply replicates AWS reported state, nothing  more.
 const fs = require('fs');
 const path = require('path');
+const lockFile = require('lockfile');
 
 const configFile = path.join(__dirname, '../../shc/config/esp-status.json');
+const lockName = 'esp-status.lock';
 
 // Retrieve REST object representing current ESP state
 async function getState()
@@ -24,10 +26,16 @@ async function updateCachedState(updatedState)
         let combinedState = { ...currentState, ...updatedState }; // NB! shallow merge
 
         return new Promise((resolved, rejected) => {
-                fs.writeFile(configFile, JSON.stringify(combinedState), (err) => {
+                lockFile.lock(lockName, function (err) {
                         if (err) rejected(err);
-                        resolved(combinedState);
+                        fs.writeFile(configFile, JSON.stringify(combinedState), (err) => {
+                                lockFile.unlock(lockName);
+                                if (err) rejected(err);
+                                resolved(combinedState);
+                        });
                 });
+
+
         });
 }
 

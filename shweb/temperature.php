ba2889e9ce27855ac53bbb193b1b3876b414d585
Dennis Afanassiev <denis.afanassiev@gmail.com>
2021-01-29 19:21:04 +0300

    embed grafana take 2

--- shweb/temperature.php
+++ shweb/temperature.php
@@ -16,7 +16,14 @@
 		include 'menu.php';
 		include 'include/js.php';
 		?>
-		<iframe src="/grafana/d/X8fZOHLMk/brod?orgId=1&from=1611329939268&to=1611934739268&refresh=1h&viewPanel=2"></iframe>
+		<div class="embed-responsive">
+			<iframe 
+				class="embed-responsive-item" 
+				width="100%" height="800"
+				allowfullscreen 
+				src="https://ec2-18-184-115-169.eu-central-1.compute.amazonaws.com/grafana/d/X8fZOHLMk/brod?orgId=1&from=1611329939268&to=1611934739268&refresh=1h&viewPanel=2">
+			</iframe>
+		</div>
 	</div>
 </body>
 </html>

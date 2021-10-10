82255e1a0d309712f6cfa13cf24732e7c0957e4d
Dennis Afanassiev <denis.afanassiev@gmail.com>
2021-10-10 21:20:27 +0300

    gateway relay reset fix

--- shweb/API/1.2/services/gateways.js
+++ shweb/API/1.2/services/gateways.js
@@ -3,15 +3,20 @@ const ShWadeAPI = require('../../../../houseAPI/shwadeAPI');
 
 let houseAPI = new ShWadeAPI(config.houseAPIorigin);
 
-const GATE_DELAY = 1000;
+const GATE_DELAY = 20000; // 20 sec
+
+function sleep(ms) 
+{
+        return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 async function Open(gatewayName)
 {
         if ('gateA' === gatewayName) {
-                const updateRequest = { zigbee : { switches : { gateAOpenSignal: 1 } } };
+                const updateRequest = { zigbee : { switches : { gateACloseSignal: 0, gateAOpenSignal: 1 } } };
                 console.log(updateRequest);
                 await houseAPI.updateStatus(updateRequest);
-                //await sleep(GATE_DELAY);
+                await sleep(GATE_DELAY);
                 updateRequest.zigbee.switches.gateAOpenSignal = 0;
                 return await houseAPI.updateStatus(updateRequest);
         }
@@ -20,9 +25,9 @@ async function Open(gatewayName)
 async function Close(gatewayName)
 {
         if ('gateA' === gatewayName) {
-                const updateRequest = { zigbee : { switches : { gateACloseSignal: 1 } } };
+                const updateRequest = { zigbee : { switches : { gateAOpenSignal: 0, gateACloseSignal: 1 } } };
                 await houseAPI.updateStatus(updateRequest);
-                //await sleep(GATE_DELAY);
+                await sleep(GATE_DELAY);
                 updateRequest.zigbee.switches.gateACloseSignal = 0;
                 return await houseAPI.updateStatus(updateRequest);
         }

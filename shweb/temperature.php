bb2836d4799bfb1957df45555bb7a7c684471b43
Dennis Afanassiev <denis.afanassiev@gmail.com>
2022-11-22 22:40:51 +0400

    ec2 instance fix

--- shweb/temperature.php
+++ shweb/temperature.php
@@ -21,7 +21,7 @@
 				class="embed-responsive-item" 
 				width="100%" height="760"
 				allowfullscreen 
-				src="https://ec2-18-184-115-169.eu-central-1.compute.amazonaws.com/grafana/d/X8fZOHLMk/panel-monitoringa?orgId=1&refresh=1h&from=now-7d&to=now">
+				src="https://ec2-3-68-66-142.eu-central-1.compute.amazonaws.com/grafana/d/X8fZOHLMk/panel-monitoringa?orgId=1&refresh=1h&from=now-7d&to=now">
 			</iframe>
 		</div>
 	</div>

666a280bd9a72353879a71574d194a62e209e29b
Dennis Afanassiev <denis.afanassiev@gmail.com>
2023-07-28 23:38:12 +0300

    updated prod URI

--- shweb/include/api.js
+++ shweb/include/api.js
@@ -2,7 +2,7 @@ function GetAPIURL(methodURI)
 {
 	var APIOptions = {
 		debugOrigin: 'http://192.168.1.162:3001',
-		prodOrigin: 'http://192.168.1.162:3001',
+		prodOrigin: 'http://ec2-3-121-211-86.eu-central-1.compute.amazonaws.com:3001',
 		subdomain: 'API',
 		version: '1.2'
 	};

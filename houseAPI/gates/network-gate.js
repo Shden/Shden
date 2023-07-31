5e510587c9e895a3f540ab0bcc5f92bb89f7f94d
Dennis Afanassiev <denis.afanassiev@gmail.com>
2023-07-31 15:56:59 +0300

    no resp gives -1

--- houseAPI/gates/network-gate.js
+++ houseAPI/gates/network-gate.js
@@ -47,7 +47,7 @@ async function pingHost(hostIP)
         return new Promise((resolved, rejected) => {
                 session.pingHost(hostIP, function (error, target, sent, received) {         
                     if (error)
-                        resolved(null);
+                        resolved(-1);
                     else
                         resolved(received - sent);
                 });

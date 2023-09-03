3df1011d1826a41327ee32e770beaf44cfe5cd56
Dennis Afanassiev <denis.afanassiev@gmail.com>
2023-09-03 13:33:05 +0300

    relay tests added

--- houseAPI/test/kincony-relays-gate-tests.js
+++ houseAPI/test/kincony-relays-gate-tests.js
@@ -3,25 +3,37 @@ const should = require('should');
 
 describe('Shutters gate tests:', function() {
 
-        let x5555H = {
-            Shutters : {
-                House : {
-                    F1: { W1: 0, W2: 1, W3: 0, W4: 1, W5: 0, W6: 1, W7: 0 },
-                    F2: { W1: 1, W2: 0, W3: 1, W4: 0, W5: 1, W6: 0, W7: 1, W8: 0, W9: 1 }
+        let houseShutters01 = {
+                Shutters: {
+                        House: {
+                                F1: { W1: 0, W2: 1, W3: 0, W4: 1, W5: 0, W6: 1, W7: 0 },
+                                F2: { W1: 1, W2: 0, W3: 1, W4: 0, W5: 1, W6: 0, W7: 1, W8: 0, W9: 1 }
+                        }
                 }
-            }
-        };
-        
-        let xAAAAH = {
-            Shutters : {
-                House : {
-                    F1: { W1: 1, W2: 0, W3: 1, W4: 0, W5: 1, W6: 0, W7: 1 },
-                    F2: { W1: 0, W2: 1, W3: 0, W4: 1, W5: 0, W6: 1, W7: 0, W8: 1, W9: 0 }
-                }
-            }
         };
 
-        let partialUpdate = {
+        let houseShutters10 = {
+                Shutters: {
+                        House: {
+                                F1: { W1: 1, W2: 0, W3: 1, W4: 0, W5: 1, W6: 0, W7: 1 },
+                                F2: { W1: 0, W2: 1, W3: 0, W4: 1, W5: 0, W6: 1, W7: 0, W8: 1, W9: 0 }
+                        }
+                }
+        };
+
+        let garageRelays01 = {
+                Relays: {
+                        Garage: { R1: 0, R2: 1, R3: 0, R4: 1, R5: 0, R6: 1, R7: 0, R8: 1, R9: 0, R10: 1, R11: 0, R12: 1, R13: 0 }
+                }
+        };
+
+        let garageRelays10 = {
+                Relays: {
+                        Garage: { R1: 1, R2: 0, R3: 1, R4: 0, R5: 1, R6: 0, R7: 1, R8: 0, R9: 1, R10: 0, R11: 1, R12: 0, R13: 1 }
+                }
+        };
+
+        let shutters_partialUpdate = {
             Shutters : {
                 House : {
                     F2: { W5: 1 }
@@ -74,12 +86,24 @@ describe('Shutters gate tests:', function() {
                 return s.getStatus().then((res) => { console.log(res)});
         });
 
-        it.skip('Can update all', function() {
-                return canSetTo(x5555H);
+        it.skip('Can update house shutters to 0101010...', function() {
+                return canSetTo(houseShutters01);
         });
 
-        it.skip('Can update parially', function() {
-                return canSetTo(partialUpdate);
+        it.skip('Can update house shutters to 1010101...', function() {
+                return canSetTo(houseShutters10);
+        });
+
+        it.skip('Can update garage relays to 0101010...', function() {
+                return canSetTo(garageRelays01);
+        });
+
+        it.skip('Can update garage relays to 1010101...', function() {
+                return canSetTo(garageRelays10);
+        });
+
+        it.skip('Can update shutters parially', function() {
+                return canSetTo(shutters_partialUpdate);
         });
 
 });

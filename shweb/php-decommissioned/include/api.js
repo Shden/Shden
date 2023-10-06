68358d9c1c67170d9f5ce37103ad0b66accc9f28
Dennis Afanassiev <denis.afanassiev@gmail.com>
2023-10-06 18:46:52 +0300

    PHP moved to decommissioned temp directory

---
+++ shweb/php-decommissioned/include/api.js
@@ -0,0 +1,16 @@
+function GetAPIURL(methodURI)
+{
+	var APIOptions = {
+		debugOrigin: 'http://localhost:3001',
+		prodOrigin: 'http://192.168.1.162:3001',
+		subdomain: 'API',
+		version: '1.2'
+	};
+
+	var debugMode = false;
+
+	var subSegments = '/' + APIOptions.subdomain + '/' + APIOptions.version + '/' + methodURI;
+	return debugMode
+		? APIOptions.debugOrigin + subSegments
+		: APIOptions.prodOrigin + subSegments;
+}

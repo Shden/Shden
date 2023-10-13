af02144e666bb0dbab855fbf5c05ea9b3ba48327
Dennis Afanassiev <denis.afanassiev@gmail.com>
2023-10-13 14:48:11 +0300

    watering schematics

--- shweb-react-app/src/Menu.js
+++ shweb-react-app/src/Menu.js
@@ -24,16 +24,19 @@ function Menu() {
                                         <Nav.Link href="/Shutters">Роллеты</Nav.Link>
                                         <Nav.Link href="/Relays">Реле</Nav.Link>
                                         <Nav.Link href="#lighting">Ворота</Nav.Link>
-                                        <NavDropdown title="Электроэнергия" id="basic-nav-dropdown">
+                                        <NavDropdown title="Электричество" id="basic-nav-dropdown">
                                                 <NavDropdown.Item href="/PowerMeter">Электросчетчик</NavDropdown.Item>
                                                 <NavDropdown.Item href="#temperature">Статистика</NavDropdown.Item>
                                                 <NavDropdown.Item href="/PowerMonitor">Монитор</NavDropdown.Item>
                                                 <NavDropdown.Divider />
-                                                <NavDropdown.Item href="/data/Электроснабжение-схема-Дом щиток-схема-12.10.2023.pdf">Схема входного щитка в доме</NavDropdown.Item>
+                                                <NavDropdown.Item href="/data/electricity/Электроснабжение-схема-Дом щиток-схема-12.10.2023.pdf">Схема входного щитка в доме</NavDropdown.Item>
                                         </NavDropdown>
                                         <NavDropdown title="Полив" id="basic-nav-dropdown">
                                                 <NavDropdown.Item href="#schedule">Монитор системы полива</NavDropdown.Item>
                                                 <NavDropdown.Item href="https://app.hydrawise.com/config/dashboard">Hydrawise</NavDropdown.Item>
+                                                <NavDropdown.Divider />
+                                                <NavDropdown.Item href="/data/watering/План дома 2020-Ландшафт Г5+МБ8-полив.pdf">Схема полива 1</NavDropdown.Item>
+                                                <NavDropdown.Item href="/data/watering/layout0 (5).pdf">Схема полива 2</NavDropdown.Item>
                                         </NavDropdown>
                                         <Nav.Link href="https://ec2-3-74-4-26.eu-central-1.compute.amazonaws.com/API/1.2/status/HouseStatus">House API</Nav.Link>
                                 </Nav>
